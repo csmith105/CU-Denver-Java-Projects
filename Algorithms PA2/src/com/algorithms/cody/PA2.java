9e458a8464431a10870a1e701445a754611a2260
Cody Smith <smartkid101@gmail.com>
2015-10-09 20:15:39 -0600

    Algorithms PA2 Finished

--- Algorithms PA2/src/com/algorithms/cody/PA2.java
+++ Algorithms PA2/src/com/algorithms/cody/PA2.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class PA2 {
 
 	public static void main(String[] args) {
 		
-		// Was a file passed to this program via command line arguements?
+		// Was a file passed to this program via command line arguments?
 		if(args.length == 0) {
 			
 			System.out.println("No arguements provided.");
@@ -34,16 +37,17 @@ public class PA2 {
 		
 		System.out.println("All lines read from file: " + args[0] + "\r\n");
 		
-		System.out.println("Begin Vector:\r\n");
-		
-		// 1. Using a vector
 		long assignmentCount = 0;
 		int wordCounter = 0;
+		
+		// 1. Begin - Using am unsorted vector
+		System.out.println("Starting Unsorted Vector...");
 		
 		ArrayList<Word> vectorWordConcordance = new ArrayList<Word>(1020000);
 		
 		// For each line in lines, try to create a word, store the successes into the vectorWordList
 		for(String string : lines) {
+			
 			Word word = Word.buildWordFromString(string);
 			
 			if(word != null) {
@@ -66,23 +70,173 @@ public class PA2 {
 					vectorWordConcordance.add(word);
 				}
 					
-				
 			}
 			
 			++wordCounter;
+				
+		}
+
+		System.out.println("Processed: " + wordCounter + " words");
+		System.out.println("Found " + vectorWordConcordance.size() + " unique words.");
+		System.out.println("In " + Word.getComparisons() + " comparisons.");
+		System.out.println("In " + assignmentCount + " assignments.");
+		System.out.println();
+		
+		// 1. End
+		
+		// Cleanup
+		Word.resetComparisons();
+		
+		assignmentCount = 0;
+		wordCounter = 0;
+		
+		// 2. Using a sorted list
+		System.out.println("Starting Sorted List...");
+		
+		LinkedList<Word> listWordConcordance = new LinkedList<Word>();
+		
+		// For each line in lines, try to create a word, store the successes into the vectorWordList
+		for(String string : lines) {
 			
-			if(wordCounter % 1000 == 0) {
-				System.out.println("Found " + vectorWordConcordance.size() + " words.");
-				System.out.println("In " + Word.getComparisons() + " comparisons.");
-				System.out.println("In " + assignmentCount + " assignments.");
+			Word word = Word.buildWordFromString(string);
+			
+			if(word != null) {
+				
+				boolean foundWord = false;
+				
+				// Search the list, looking for matching words
+				ListIterator<Word> itr = listWordConcordance.listIterator();
+				
+				while(itr.hasNext()) {
+					
+					Word listword = itr.next();
+					
+					if(listword.compareTo(word) == 0) {
+						listword.incrementCount();
+						foundWord = true;
+						break;
+					}
+					
+					// See if this is where our word goes in the list
+					if(itr.hasNext()) {
+						
+						// Peek the next word
+						Word nextListWord = itr.next();
+						itr.previous();
+						
+						if(word.compareTo(listword) > 0 && word.compareTo(nextListWord) < 0) {
+							++assignmentCount;
+							itr.add(word);
+							foundWord = true;
+							break;
+						}
+						
+					}
+					
+				}
+				
+				// If the word wasn't found, add it to the end
+				if(foundWord == false) {
+					++assignmentCount;
+					listWordConcordance.add(word);
+				}
+					
 			}
+			
+			++wordCounter;
 				
 		}
 		
-		System.out.println("FINAL: Found " + vectorWordConcordance.size() + " words.");
-		System.out.println("FINAL: In " + Word.getComparisons() + " comparisons.");
-		System.out.println("FINAL: In " + assignmentCount + " assignments.");
+		System.out.println("Processed: " + wordCounter + " words");
+		System.out.println("Found " + listWordConcordance.size() + " unique words.");
+		System.out.println("In " + Word.getComparisons() + " comparisons.");
+		System.out.println("In " + assignmentCount + " assignments.");
+		System.out.println();
 
+		// 2. End
+		
+		// Cleanup
+		Word.resetComparisons();
+		
+		assignmentCount = 0;
+		wordCounter = 0;
+		
+		// 3. Using a BTree
+		System.out.println("Starting Sorted BTree...");
+		
+		BTree treeWordConcordance = new BTree();
+		
+		// For each line in lines, try to create a word, store the successes into the vectorWordList
+		for(String string : lines) {
+			
+			Word word = Word.buildWordFromString(string);
+			
+			if(word != null) {
+				
+				Word foundWord = treeWordConcordance.find(word);
+				
+				if(foundWord != null) {
+					foundWord.incrementCount();
+				} else {
+					treeWordConcordance.insert(word);
+					++assignmentCount;
+				}
+					
+			}
+			
+			++wordCounter;
+				
+		}
+		
+		System.out.println("Processed: " + wordCounter + " words");
+		System.out.println("Found " + treeWordConcordance.size() + " unique words.");
+		System.out.println("In " + Word.getComparisons() + " comparisons.");
+		System.out.println("In " + assignmentCount + " assignments.");
+		System.out.println();
+
+		// 3. End
+		
+		// Cleanup
+		Word.resetComparisons();
+		
+		assignmentCount = 0;
+		wordCounter = 0;
+		
+		// 4. Using a Hashtable
+		System.out.println("Starting Sorted Hashtable...");
+		
+		Hashtable<String, Word> hashtable = new Hashtable<String, Word>();
+		
+		// For each line in lines, try to create a word, store the successes into the vectorWordList
+		for(String string : lines) {
+			
+			Word word = Word.buildWordFromString(string);
+			
+			if(word != null) {
+				
+				boolean foundWord = hashtable.containsKey(word.getString());
+				
+				if(foundWord) {
+					hashtable.get(word.getString()).incrementCount();
+				} else {
+					hashtable.put(word.getString(), word);
+					++assignmentCount;
+				}
+					
+			}
+			
+			++wordCounter;
+				
+		}
+		
+		System.out.println("Processed: " + wordCounter + " words");
+		System.out.println("Found " + hashtable.size() + " unique words.");
+		System.out.println("In " + Word.getComparisons() + " comparisons.");
+		System.out.println("In " + assignmentCount + " assignments.");
+		System.out.println();
+		
+		// 4. End
+				
 	}
 
 }
