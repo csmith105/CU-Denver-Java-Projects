f69337704e616742e9e30e663124c3946726956c
Cody Smith <smartkid101@gmail.com>
2015-10-18 00:18:22 -0600

    PA5 Finished

--- Advanced PA5/src/PA5.java
+++
@@ -1,9 +0,0 @@
-
-public class PA5 {
-
-	public static void main(String[] args) {
-		
-
-	}
-
-}
---
+++ Advanced PA5/src/com/advanced/cody/PA5.java
@@ -0,0 +1,15 @@
+package com.advanced.cody;
+
+public class PA5 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Program Started.");
+		
+		DiningPhilosophers foo = new DiningPhilosophers();
+		
+		System.out.println("Program Ended.");
+		
+	}
+
+}
